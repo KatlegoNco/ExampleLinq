e77406f85dd7541709c4f64f0e4556f6405e6b85
Grealish10k <katlegoncongwane20@gmail.com>
2024-05-22 10:28:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string> { "Dog", "Cat", "Snake", "Rabbit", "Bird" };
+
+            // Filter animals at index 3 (which is "Rabbit")
+            var animalQuery =
+                from animal in animals
+                where animals.IndexOf(animal) == 1
+                select animal;
+
+            foreach (var animal in animalQuery)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
